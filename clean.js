305569df8cc52ee83878dbed827a36f29e4b1ecf
0NYX07 <krishnasahnionyx@gmail.com>
2026-04-19 17:58:50 +0530

    Add files via upload

---
+++ clean.js
@@ -0,0 +1,42 @@
+const fs = require('fs');
+const path = require('path');
+
+const srcDir = path.join(__dirname, 'src');
+
+function cleanFile(filePath) {
+  let content = fs.readFileSync(filePath, 'utf8');
+  
+  const original = content;
+
+  // 1. Strip JSX comments
+  content = content.replace(/\{\s*\/\*[\s\S]*?\*\/\s*\}/g, '');
+  
+  // 2. Strip block comments
+  content = content.replace(/\/\*[\s\S]*?\*\//g, '');
+
+  // 3. Strip full-line comments starting with //
+  content = content.replace(/^[ \t]*\/\/.*$/gm, '');
+
+  // 4. Strip inline comments starting with // (be careful with URLs)
+  content = content.replace(/[ \t]+\/\/.*$/gm, '');
+
+  if (original !== content) {
+    fs.writeFileSync(filePath, content, 'utf8');
+    console.log('Cleaned:', filePath);
+  }
+}
+
+function processDirectory(dir) {
+  const files = fs.readdirSync(dir);
+  for (const file of files) {
+    const fullPath = path.join(dir, file);
+    if (fs.statSync(fullPath).isDirectory()) {
+      processDirectory(fullPath);
+    } else if (/\.(js|jsx|css)$/.test(file)) {
+      cleanFile(fullPath);
+    }
+  }
+}
+
+processDirectory(srcDir);
+console.log('Finished removing comments');
